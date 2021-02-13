020541cd02602b19b189373338e7bf96cd39fd01
SheepTester <ussa.say@gmail.com>
2021-02-12 20:50:26 -0800

    Use old discord.js web build because discord.com domain has CloudFlare but discordapp.com still works (despite being deprecated) D:<

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 import { getToken } from './token.js'
 
-getToken({
-  storageKey: '[discordjackbox] bot token',
-  form: document.getElementById('token-form'),
-  input: document.getElementById('token-input'),
-  saveCheckbox: document.getElementById('token-save')
-}).then(token => {
-  document.getElementById('token-form').remove()
-})
+const client = new Discord.Client()
+
+async function main () {
+  client.on('ready', () => {
+    console.log(client)
+  })
+
+  const tokenForm = document.getElementById('token-form')
+  const token = await getToken({
+    storageKey: '[discordjackbox] bot token',
+    form: tokenForm,
+    input: document.getElementById('token-input'),
+    saveCheckbox: document.getElementById('token-save')
+  })
+  tokenForm.remove()
+  await client.login(token)
+}
+
+main()
