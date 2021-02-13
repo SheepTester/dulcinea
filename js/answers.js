fe3b563be96b4bb1a6d8a71a8c8d1daf9fc928f8
Alexander Chang-Davidson <changdavidson4@gmail.com>
2021-02-13 14:47:58 -0800

    expectAnswers to collect answers in the next x ms

--- js/answers.js
+++ js/answers.js
@@ -1,5 +1,14 @@
 const answers = new Map()
 
+export async function expectAnswers(channel, time) {
+    const messages = await Promise.allSettled(channel.members.map(async member => {
+        const dmChannel = await member.createDM()
+        //await dmChannel.send('')
+        return dmChannel.awaitMessages(() => true, { max: 1, time, errors: ['time'] }).then(messages => messages.first())
+    })).then(results => results.filter(res => res.status === 'fulfilled').map(res => res.value))
+    return new Map(channel.members.map(m => [m.id, messages.find(mess => mess.author.id === m.id)])) // only returns answers for members still in vc
+}
+
 export function collectAnswer(message, voiceChannel) {
     if (message.guild || message.author.bot || !voiceChannel.members.has(message.author.id)) return
     if (answers.has(message.author.id)) {
