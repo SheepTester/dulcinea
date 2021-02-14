0c25cf6e1d75cd93ba5c0499a64ec40b48fdf057
SheepTester <ussa.say@gmail.com>
2021-02-13 19:12:39 -0800

    Implement minimal game

--- js/games/test.js
+++ js/games/test.js
@@ -10,6 +10,23 @@ for (let i = 0x1f1e6; i <= 0x1f1ff; i++) {
   emoji.push(String.fromCodePoint(i))
 }
 
+const voteMessages = [
+  'Voting time! Reply with either `A` or `B`.',
+  'Reply with either `A` or `B` to vote.',
+  'It\'s time to vote! Reply with either `A` or `B`.'
+]
+
+const defaultResponses = [
+  'Ugh.',
+  'I don\'t know.',
+  'This is too big brain for me.',
+  'lol',
+  'I disagree.'
+]
+function autoResponse () {
+  return defaultResponses[Math.random() * defaultResponses | 0]
+}
+
 const questionsPromise = fetch(new URL('../questions.json', import.meta.url))
   .then(response => response.json())
 
@@ -25,31 +42,63 @@ function Game ({ channel, onEnd }) {
         .filter(member => !member.user.bot)
         .keys()
     ))
-    const pairs = new Map() // Question => UserId[]
+    const scores = new Map(memberIds.map(userId => [userId, 0])) // UserId => number
     const prompts = new Map(memberIds.map(userId => [userId, []])) // UserId => Question[]
     for (let i = 0; i < memberIds.length; i++) {
       const [prompt] = questions.splice(Math.floor(Math.random() * questions.length), 1)
       if (i === 0) {
         prompts.get(memberIds[0]).push(prompt)
         prompts.get(memberIds[memberIds.length - 1]).push(prompt)
-        pairs.set(prompt, [memberIds[0], memberIds[memberIds.length - 1]])
       } else {
         prompts.get(memberIds[i]).push(prompt)
         prompts.get(memberIds[i - 1]).push(prompt)
-        pairs.set(prompt, [memberIds[i], memberIds[i - 1]])
       }
     }
 
     const answerExpecter = new Answers(channel)
-    // setEndTime(Date.now() + 10000)
-    // const answers = await answerExpecter.expectAnswers(10000)
-    // setAnswers(Array.from(answers.entries()))
+
     setEndTime(Date.now() + 10000)
-    const votes = await answerExpecter.expectVotes(10000, emoji.slice(0, 3))
-    setVotes(Array.from(votes.entries()))
-    setEndTime(null)
-    await wait(10000)
-    onEnd()
+    const answers = await answerExpecter.expectAnswers(10000, prompts)
+    setAnswers(Array.from(answers.entries()))
+
+    const responses = new Map() // Question => [UserId, string][]
+    for (const [userId, userPrompts] of prompts) {
+      const userAnswers = answers.get(userId) || []
+      for (let i = 0; i < userPrompts.length; i++) {
+        const prompt = userPrompts[i]
+        const answer = userAnswers[i]
+        let promptResponses = responses.get(prompt)
+        if (!promptResponses) {
+          promptResponses = []
+          responses.set(prompt, promptResponses)
+        }
+        promptResponses.push([userId, answer])
+      }
+    }
+
+    console.log(responses)
+    for (const [
+      prompt,
+      [[userA, answerA = autoResponse()], [userB, answerB = autoResponse()]]
+    ] of responses) {
+      console.log(prompt, userA, answerA, userB, answerB)
+      setEndTime(Date.now() + 10000)
+      const votes = await answerExpecter.expectAnswers(
+        10000,
+        new Map(memberIds.map(userId => [userId, [voteMessages[Math.random() * voteMessages.length | 0]]]))
+      )
+      setEndTime(null)
+      for (const [vote = ''] of votes.values()) {
+        if (vote.toLowerCase() === 'a') {
+          scores.set(userA, scores.get(userA) + 500)
+        } else if (vote.toLowerCase() === 'b') {
+          scores.set(userB, scores.get(userB) + 500)
+        }
+      }
+      console.log(scores)
+      await wait(10000)
+    }
+    // onEnd()
   }, [onEnd])
 
   return e(
@@ -65,7 +114,7 @@ function Game ({ channel, onEnd }) {
     ),
     answers && answers.map(([userId, message]) => (
       message
-        ? e('p', { key: userId }, `${message.author.tag} says, "${message.content}"`)
+        ? e('p', { key: userId }, `${channel.client.users.cache.get(userId).tag} says, "${message.join(', ')}"`)
         : e('p', { key: userId }, `${channel.client.users.cache.get(userId).tag} didn't say anything.`)
     )),
     votes && votes.map(([userId, vote]) => (
